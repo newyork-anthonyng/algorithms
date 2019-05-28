e58a4296a99b2907199f02e6449bb9a177c93115
Anthony Ng <newyork.anthonyng@gmail.com>
2019-05-28 19:06:01 -0400

    Add 03_urlify

---
+++ 01_arrays_and_strings/03_urlify.js
@@ -0,0 +1,6 @@
+function urlify(string) {
+  return string.split(" ").join("%20");
+}
+
+console.assert(urlify("Ms. Jones") === "Ms.%20Jones", "Ms. Jones was not urlify'd correctly");
+console.assert(urlify("Ms. Jones is great") === "Ms.%20Jones%20is%20great", "Ms. Jones was not urlify'd correctly");
