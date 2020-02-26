ea4b1e4ad35c07557517d1093cdb2939f9d70e1f
Anthony Ng <newyork.anthonyng@gmail.com>
2020-02-26 12:46:10 -0500

    use example

--- leetcode/0009_palindrome_number.js
+++ leetcode/0009_palindrome_number.js
@@ -28,3 +28,24 @@ function isPalindrome2(x) {
 console.assert(isPalindrome2(121) === true, '121');
 console.assert(isPalindrome2(-121) === false, '-121');
 console.assert(isPalindrome2(10) === false, '10');
+
+/*
+x === 121
+tmp === 121
+invx === 0
+
+invx = (0 * 10) + (121 % 10) => 0 + (1) => 1
+invx === 1
+tmp = Math.floor(121 / 10) => 12
+tmp === 12
+
+invx = (1 * 10) + (12 % 10) => 10 + 2 => 12
+invx === 12
+tmp = Math.floor(12 / 10) => 1
+tmp === 1
+
+invx = (12 * 10) + (1 % 10) => 120 + 1 => 121
+invx === 121
+tmp = Math.floor(1 / 10) => 0
+tmp === 0
+*/
