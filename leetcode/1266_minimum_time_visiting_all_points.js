52b117127c6fd8b2f622bcb545b1d673bba1569d
Anthony Ng <newyork.anthonyng@gmail.com>
2020-04-01 12:29:20 -0400

    add minimum time visiting all points

---
+++ leetcode/1266_minimum_time_visiting_all_points.js
@@ -0,0 +1,30 @@
+function minTimeToVisitAllPoints(points) {
+  let counter = 0;
+
+  let currentPoint = points[0];
+  for (let i = 1; i < points.length; i++) {
+    let xDiff = Math.abs(currentPoint[0] - points[i][0]);
+    let yDiff = Math.abs(currentPoint[1] - points[i][1]);
+
+    while (xDiff !== 0 && yDiff !== 0) {
+      xDiff--;
+      yDiff--;
+      counter++;
+    }
+
+    counter += xDiff;
+    counter += yDiff;
+
+    currentPoint = points[i];
+  }
+
+  return counter;
+}
+
+let input = [[1, 1], [3, 4], [-1, 0]];
+let output = minTimeToVisitAllPoints(input);
+console.log(output);
+
+input = [[3, 2], [-2, 2]];
+output = minTimeToVisitAllPoints(input);
+console.log(output);
